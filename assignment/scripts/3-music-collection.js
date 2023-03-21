db0c9c09d6ac06e90e25a96f0489ef1a1a7ce886
Luis Ortiz <l.ortizintec@gmail.com>
2023-03-21 16:34:44 -0400

    showCollection tested and proven

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,20 +1,23 @@
 console.log('***** Music Collection *****');
 
-
-
 let collection = [];
-
 
 function addToCollection(title, artist, yearPublished){
 
-let song = {
+let album = {
     title: title,
     artist: artist,
     yearPublished: yearPublished
 };
 
-collection.push(song);
+collection.push(album);
 console.log(collection[collection.length - 1]);
+}
+
+function showCollection(array){
+    for(i=0; i < array.length; i++){
+        console.log(array[i].title + ' by ' + array[i].artist + ' published in ' + array[i].yearPublished + '.');
+    }
 }
 
 addToCollection('Yellow', 'Coldplay', 2000);
@@ -24,3 +27,4 @@ addToCollection('Candy Paint', 'Post Malone', 2018);
 addToCollection('Wishing Well', 'Juice WRLD', 2020);
 addToCollection('Righteous', 'Juice WRLD', 2020);
 console.log(collection);
+showCollection(collection);
