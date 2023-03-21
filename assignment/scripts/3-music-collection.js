7ff8a3d90116a16163dc51fa7fd21f2fa667baf2
Luis Ortiz <l.ortizintec@gmail.com>
2023-03-21 12:49:35 -0400

    added albums and tested addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,26 @@
-console.log('***** Music Collection *****')
+console.log('***** Music Collection *****');
+
+
+
+let collection = [];
+
+
+function addToCollection(title, artist, yearPublished){
+
+let song = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+};
+
+collection.push(song);
+console.log(collection[collection.length - 1]);
+}
+
+addToCollection('Yellow', 'Coldplay', 2000);
+addToCollection('The Scientist', 'Coldplay', 2002);
+addToCollection('White Iverson', 'Post Malone', 2016);
+addToCollection('Candy Paint', 'Post Malone', 2018);
+addToCollection('Wishing Well', 'Juice WRLD', 2020);
+addToCollection('Righteous', 'Juice WRLD', 2020);
+console.log(collection);
